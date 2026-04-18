a4ab68971313b640d4faccbc2eae9126f91b6942
SylvaticGhost <k.tkachov05@gmail.com>
2026-04-19 01:03:59 +0300

    Update RegistrySchema.sql
    w3

--- api/sql/RegistrySchema.sql
+++ api/sql/RegistrySchema.sql
@@ -2,32 +2,32 @@ CREATE SCHEMA IF NOT EXISTS registry;
 
 CREATE TABLE IF NOT EXISTS registry.land (
   cadastral_number          VARCHAR(22)        PRIMARY KEY,
-  koatuu                    VARCHAR(10)        NOT NULL,
-  ownership_type            VARCHAR(100)       NOT NULL,
-  intended_purpose          TEXT               NOT NULL,
-  location                  TEXT               NOT NULL,
-  land_purpose_type         VARCHAR(255)       NOT NULL,
-  square                    NUMERIC(12, 4)     NOT NULL,
-  estimate_value            NUMERIC(15, 2)     NOT NULL,
-  state_tax_id              VARCHAR(10)        NOT NULL,
-  "user"                    VARCHAR(255)       NOT NULL,
+  koatuu                    text        ,
+  ownership_type            VARCHAR(255)       ,
+  intended_purpose          TEXT               ,
+  location                  TEXT               ,
+  land_purpose_type         VARCHAR(255)       ,
+  square                    NUMERIC(12, 4)     ,
+  estimate_value            NUMERIC(15, 2)     ,
+  state_tax_id              text        ,
+  "user"                    VARCHAR(255)       ,
   owner_part                NUMERIC(5, 4),
-  state_registration_date   DATE               NOT NULL,
-  ownership_registration_id VARCHAR(100)       NOT NULL,
-  registrator               VARCHAR(255)       NOT NULL,
-  type                      VARCHAR(100)       NOT NULL,
-  subtype                   VARCHAR(100)       NOT NULL
+  state_registration_date   DATE               ,
+  ownership_registration_id VARCHAR(255)       ,
+  registrator               VARCHAR(255)       ,
+  type                      VARCHAR(255)       ,
+  subtype                   VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS registry.realty (
-  state_tax_id                    VARCHAR(10)        NOT NULL,
-  taxpayer_name                   VARCHAR(255)       NOT NULL,
-  object_type                     VARCHAR(100)       NOT NULL,
-  object_address                  TEXT               NOT NULL,
-  ownership_registration_date     DATE               NOT NULL,
+  state_tax_id                    text        ,
+  taxpayer_name                   VARCHAR(255)       ,
+  object_type                     VARCHAR(255)       ,
+  object_address                  TEXT               ,
+  ownership_registration_date     DATE               ,
   ownership_termination_date      DATE,
-  total_area                      NUMERIC(12, 4)     NOT NULL,
-  joint_ownership_type            VARCHAR(100),
+  total_area                      NUMERIC(12, 4)     ,
+  joint_ownership_type            VARCHAR(255),
   ownership_share                 NUMERIC(5, 4),
   PRIMARY KEY (state_tax_id, ownership_registration_date)
 );
